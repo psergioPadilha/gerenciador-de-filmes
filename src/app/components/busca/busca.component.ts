348e22ade12b67603161ff147d11b82ffc519230
psergioPadilha <163341752+psergioPadilha@users.noreply.github.com>
2024-10-07 00:49:16 -0300

    Implementação da Busca de filmes

--- src/app/components/busca/busca.component.ts
+++ src/app/components/busca/busca.component.ts
@@ -2,28 +2,59 @@ import { Component } from '@angular/core';
 import { FilmeService } from '../../services/filme.service';
 import { ResultadoBuscaFilme } from '../../models/resultado-busca-filme.model';
 import { ListagemFilme } from '../../models/listagem-filme.model';
-import { formatDate } from '@angular/common';
+import { formatDate, NgClass, NgForOf, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-busca',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgForOf, RouterLink, NgClass],
   templateUrl: './busca.component.html',
   styleUrl: './busca.component.scss'
 })
 
 export class BuscaComponent {
   public resultadoBusca?: ResultadoBuscaFilme;
+  public maximoPaginaAlcancada: boolean;
 
-  constructor(private filmeService: FilmeService) { }
+  constructor(private filmeService: FilmeService) {
+    this.maximoPaginaAlcancada = false;
+  }
 
-  public buscar(query: string): void {
+  public buscar(query: string, pagina: number = 1): void {
     if(query.length < 1)
       return;
 
-    this.filmeService.buscarFilmes(query).subscribe(res => {
-      this.resultadoBusca = this.mapearResultadoBusca(res);
+    this.maximoPaginaAlcancada = false;
+
+    this.filmeService.buscarFilmes(query, pagina).subscribe(res => {
+      const novoResultado = this.mapearResultadoBusca(res);
+
+      if(this.resultadoBusca) {
+        if (pagina >= this.resultadoBusca?.quantidadePaginas){
+          this.maximoPaginaAlcancada = true;
+        }
+
+        this.resultadoBusca.pagina = novoResultado.pagina;
+        this.resultadoBusca.filmes.push(...novoResultado.filmes)
+      }
+      else {
+        this.resultadoBusca = novoResultado;
+      }
     });
+  }
+
+  public mapearCorDaNota(porcentagemNota: string): string {
+    const numeroNota = Number(porcentagemNota);
+
+    if ((numeroNota >= 0) && (numeroNota <= 30))
+      return 'app-borda-nota-mais-baixa';
+    else if ((numeroNota > 30) && (numeroNota <= 50))
+      return 'app-borda-nota-baixa';
+    else if ((numeroNota > 50) && (numeroNota <= 75))
+      return 'app-borda-nota-media';
+    else
+      return 'app-borda-nota-alta';
   }
 
   private mapearResultadoBusca(obj: any): ResultadoBuscaFilme {
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  public colapsarNavbar: boolean;
 
+  constructor() {
+    this.colapsarNavbar = false;
+  }
 }
